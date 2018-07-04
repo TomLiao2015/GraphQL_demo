61fadbc11a36f88554697b981b9fc82f5ca9d16b
Tom Liao <tom.liao@ericsson.com>
2018-07-04 12:10:03 +0800

    add graphql endpoint

--- mongodb/index.js
+++ mongodb/index.js
@@ -1,7 +1,7 @@
 const { MongoClient, ObjectId, Binary } = require('mongodb');
 
-const dbAddress = 'mongodb://localhost/demo';
-
+// const dbAddress = 'mongodb://localhost/demo';
+const dbAddress = 'mongodb://tom:1q2w3e4r@ds227821.mlab.com:27821/tomdemo';
 const MISSING_FILTER = new Error('Missing query filter argument');
 const MISSING_AGGREGATE = new Error('Missing query aggregate');
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const Koa = require('koa');
 const KoaStatic = require('koa-static');
 const bodyParser = require('koa-bodyparser');
-
+const cors = require('koa-cors');
 
 require('./mongodb');
 
@@ -13,10 +13,12 @@ const port = 4000;
 
 app.use(bodyParser());
 app.use(KoaStatic(__dirname + '/public'));
-
+// CORS
+app.use(cors({ origin: '*', allowMethods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'] }));
 
 app.use(router.routes())
   .use(router.allowedMethods());
+
 
 app.listen(port);
 
