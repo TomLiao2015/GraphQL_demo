15a60921046c480cce9fcbaf08e5efc40a765b90
Tom Liao <tom.liao@ericsson.com>
2018-07-04 12:21:51 +0800

    add graphql endpoint

--- server.js
+++ server.js
@@ -2,22 +2,26 @@ const Koa = require('koa');
 const KoaStatic = require('koa-static');
 const bodyParser = require('koa-bodyparser');
 const cors = require('koa-cors');
-
+const path = require('path');
 require('./mongodb');
 
 const router = require('./router');
 
 const app = new Koa();
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(bodyParser());
-app.use(KoaStatic(__dirname + '/public'));
+
+app.use(KoaStatic(path.join(__dirname, 'public')));
 // CORS
 app.use(cors({ origin: '*', allowMethods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'] }));
 
 app.use(router.routes())
   .use(router.allowedMethods());
+
+
+
 
 
 app.listen(port);
