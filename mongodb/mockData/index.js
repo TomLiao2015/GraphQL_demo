e7d837f70109345630dd4d9a67dd1d79d7c99f7e
Tom Liao <tom.liao@ericsson.com>
2018-07-06 15:42:56 +0800

    create mock data in database

--- mongodb/mockData/index.js
+++ mongodb/mockData/index.js
@@ -1,31 +1,59 @@
 const uuidv4 = require('uuid/v4');
 const customerId = uuidv4();
+
+function getRandomInt (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+const firstName = ['Tom', 'Alex', 'Peter', 'Susun', 'Tonny'];
+const lastName = ['Liao', 'Chen', 'Huang', 'Zhang', 'Li'];
+const countries = ['China', 'England', 'America' ];
+const currency = ['CNY', 'USD', 'GBP'];
+const customers = [];
+const transactions = [];
+function createCustomers () {
+  for (let i = 0; i < 10; i++) {
+    const customer = {
+      id: uuidv4(),
+      firstName: firstName[getRandomInt(0, 5)],
+      lastName: lastName[getRandomInt(0, 5)],
+      birthday: '1987-01-01',
+      country: countries[getRandomInt(0, 3)],
+      accountNumber: `${getRandomInt(0, 100)}-${getRandomInt(0, 100)}-${getRandomInt(0, 100)}`,
+      amount: getRandomInt(0, 5000),
+      currency: currency[getRandomInt(0, 3)]
+    };
+    customers.push(customer);
+  }
+
+}
+function createTransactions () {
+  let cunstomerRandom;
+  for (let j = 0; j < 10; j++) {
+    cunstomerRandom = getRandomInt(0, 10);
+    const transaction = {
+      id: uuidv4(),
+      customerId: customers[cunstomerRandom].id,
+      tranDay: new Date(),
+      tranAmount: getRandomInt(0, 2000),
+      tranCurrency: currency[getRandomInt(0, 3)],
+      fromAccount: customers[cunstomerRandom].accountNumber,
+      toAccount: customers[getRandomInt(0, 10)].accountNumber
+    };
+    transactions.push(transaction);
+  }
+
+}
+createCustomers();
+createTransactions();
 const mock = {
   data: {
     customers: {
-      list: [
-        {
-          id: customerId,
-          firstName: 'Tom',
-          lastName: 'Liao',
-          birthday: '1987-01-01',
-          country: 'China',
-          accountNumber: '123-456-789',
-          amount: 1000,
-          currency: 'CNY'
-        }
-      ]
+      list: customers
     },
     transactions: {
-      list: [{
-        id: uuidv4(),
-        customerId: customerId,
-        tranDay: new Date(),
-        tranAmount: 200,
-        tranCurrency: 'CNY',
-        fromAccount: '123-456-789',
-        toAccount: '125-325-755'
-      }]
+      list: transactions
     }
   }
 };
